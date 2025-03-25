3be3983b2864812a22d2fd3d5d70a2fbdf62c00c
Sukhman3125 <sukhman3125@gmail.com>
2025-03-25 15:55:17 +0530

    Added plane and plane build

---
+++ Plane/Assets/Challenge 1/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+    public PropellerRotate propellerrotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        propellerrotate = GameObject.FindGameObjectWithTag("Propeller").GetComponent<PropellerRotate>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        if (!propellerrotate.wall_collision)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
